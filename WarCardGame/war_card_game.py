c53f7a45d67ebde0472009aa02c4e5c374369018
slaveya-chonovska <slaveya.chonovska@strypes.eu>
2023-07-28 15:08:01 +0300

    Fixed bugs

--- WarCardGame/war_card_game.py
+++ WarCardGame/war_card_game.py
@@ -85,8 +85,11 @@ class WarGame:
             csv_file.writerow(("Player 1 total cards","Player 2 total cards","Player 1 card","Player 2 card","Winner","War")) #field headers
 
     def add_round_to_csv(self,msg:list) -> None:
-        """ The msg should contain in order: "Player 1 total cards","Player 2 total cards","Player 1 card","Player 2 card","Winner","War"""
-        with open(self.csv_file_name,'a',encoding='utf-8') as file:
+        """ The msg should contain in order: 
+        "Player 1 total cards","Player 2 total cards",
+        "Player 1 card","Player 2 card","Winner","War"""
+
+        with open(self.csv_file_name, 'a', encoding='utf-8') as file:
             csv_file = csv.writer(file)
             csv_file.writerow(msg)
 
@@ -141,8 +144,10 @@ class WarGame:
         print(f"Player 2: {player2_card_rank[0]}")
 
         #calculations for the csv file
-        player1_total_turn_cards = self.player1_total_cards + len(player1_face_down_cards) if war else self.player1_total_cards + 1 + len(player1_face_down_cards)
-        player2_total_turn_cards = self.player2_total_cards + len(player2_face_down_cards) if war else self.player2_total_cards + 1 + len(player2_face_down_cards)
+        player1_total_turn_cards = self.player1_total_cards + len(player1_face_down_cards) if war \
+                                   else self.player1_total_cards + 1 + len(player1_face_down_cards)
+        player2_total_turn_cards = self.player2_total_cards + len(player2_face_down_cards) if war \
+                                   else self.player2_total_cards + 1 + len(player2_face_down_cards)
         
         #if player 1's card is higher
         if player1_card_rank[1] > player2_card_rank[1]:
@@ -153,7 +158,7 @@ class WarGame:
                                    player1_card_rank[0], player2_card_rank[0], "Player 1", str(False)])
             #if this was a war round, remove/append multiple cards
             if war:
-                self.war_round_card_removal("player1",player1_face_down_cards,player2_face_down_cards)
+                self.war_round_card_removal("player1", player1_face_down_cards, player2_face_down_cards)
             else:
                 #add the won card
                 self._player1_won_stack.append(player2_card)
@@ -179,13 +184,12 @@ class WarGame:
             self.add_round_to_csv([player1_total_turn_cards, 
                                    player2_total_turn_cards,
                                    player1_card_rank[0], player2_card_rank[0], "None", str(True)])
-            self.war(player1_card,player2_card,continuing_card_index)
+            self.war(player1_card, player2_card, continuing_card_index,
+                     prev_player1_face_down_cards = player1_face_down_cards,
+                     prev_player2_face_down_cards = player2_face_down_cards)
         time.sleep(2)
-        #after continue playing
-        self.play()
     
     def war_round_card_removal(self,winner:str,player1_face_down_cards:list,player2_face_down_cards:list) -> None:
-        time.sleep(2)
 
         # if winner is player 1, add both face down stacks to his win stack
         if winner == "player1":
@@ -200,10 +204,9 @@ class WarGame:
             self._player2_won_stack += (player1_face_down_cards)
 
         time.sleep(2)
-        # war is over so continue playing
-        self.play()
 
-    def war(self,player1_card:namedtuple,player2_card:namedtuple,continuing_card_index:int) -> None:
+    def war(self,player1_card:namedtuple,player2_card:namedtuple,continuing_card_index:int,
+            prev_player1_face_down_cards:list = [],prev_player2_face_down_cards:list = []) -> None:
         # keep track of how many cards are displayed
         # start from index + 3 than the previously revealed card
 
@@ -212,6 +215,12 @@ class WarGame:
         # add the cards that started the war to the face down stacks
         player1_face_down_cards = [player1_card]
         player2_face_down_cards = [player2_card]
+
+        # also add any previous face down cards
+        if len(prev_player1_face_down_cards):
+            player1_face_down_cards.extend(prev_player1_face_down_cards)
+        if len(prev_player2_face_down_cards):
+            player2_face_down_cards.extend(prev_player2_face_down_cards)
 
         # add the top 3 cards of the deck + the 4th card,that is later used for comparison, to the face down stack
         # be wary of if a player needs to change to their win stack
